f87f805712ae7fe23bbd4c20acf6712950394bc3
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-01-31 19:27:37 +0530

    added master

---
+++ workshop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace workshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TicTacToe Game");
+
+            TicTacToegame obj = new TicTacToegame();
+            obj.GameBoard();
+        }
+
+    }
+}
+
+
---
+++ workshop/TicTacToegame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace workshop
+{
+    class TicTacToegame
+    {
+        
+        char[] Display = new char[9];
+        public void GameBoard()
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                Display[i] = ' ';
+            }
+        }
+    }
+}
